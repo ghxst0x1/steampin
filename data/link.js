a5d618572d1aeec2e9cc5295bdaf3c5e08817ffa
HoLyGhxsT <karthikrofficial7@gmail.com>
2022-10-30 16:31:48 +0530

    Major code changes

---
+++ data/link.js
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+  {
+    title: "Google",
+    url: "https://google.com/",
+    desc: "Just your typical google",
+  },
+  {
+    title: "DIM",
+    url: "https://app.destinyitemmanager.com/",
+    desc: "Destiny Item Manager",
+  },
+  {
+    title: "Today-in-destiny",
+    url: "https://www.todayindestiny.com/",
+    desc: "A summary of what's available in Destiny today.",
+  },
+  {
+    title: "D2ArmorPicker",
+    url: "https://d2armorpicker.com/",
+    desc: "A simple armor min-max tool by Mijago.",
+  },
+  {
+    title: "Destiny Recipes",
+    url: "https://destinyrecipes.com/",
+    desc: "Tools for Destiny2",
+  },
+]
