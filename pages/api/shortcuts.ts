683c05ff7bf615a3b3b1e07370599bbeba43a41e
ghxst0x1 <61968324+ghxst0x1@users.noreply.gihub.com>
2024-09-04 21:59:27 +0530

    Added new shortcuts

--- pages/api/shortcuts.ts
+++ pages/api/shortcuts.ts
@@ -1,5 +1,5 @@
 // pages/api/shortcuts.ts
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
 interface Shortcut {
   id: string;
@@ -8,15 +8,71 @@ interface Shortcut {
   group: string;
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Shortcut[]>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Shortcut[]>
+) {
   const shortcuts: Shortcut[] = [
-    { id: '1', title: 'Google', link: 'https://www.google.com', group: 'Search Engines' },
-    { id: '2', title: 'YouTube', link: 'https://www.youtube.com', group: 'Entertainment' },
-    { id: '3', title: 'GitHub', link: 'https://www.github.com', group: 'Development' },
-    { id: '4', title: 'Stack Overflow', link: 'https://stackoverflow.com', group: 'Development' },
-    { id: '5', title: 'Netflix', link: 'https://www.netflix.com', group: 'Entertainment' },
-    { id: '6', title: 'Amazon', link: 'https://www.amazon.com', group: 'Shopping' },
-    { id: '7', title: 'Twitch', link: 'https://www.twitch.tv', group: 'Entertainment' },
+    {
+      id: "1",
+      title: "Twitch",
+      link: "https://www.twitch.tv",
+      group: "Entertainment",
+    },
+    {
+      id: "2",
+      title: "R6 Tracker",
+      link: "https://r6.tracker.network",
+      group: "Games",
+    },
+    {
+      id: "3",
+      title: "DIM",
+      link: "https://app.destinyitemmanager.com",
+      group: "Games",
+    },
+    {
+      id: "4",
+      title: "Today-in-destiny",
+      link: "https://www.todayindestiny.com",
+      group: "Games",
+    },
+    {
+      id: "5",
+      title: "D2ArmorPicker",
+      link: "https://d2armorpicker.com",
+      group: "Games",
+    },
+    {
+      id: "6",
+      title: "Destiny Recipes",
+      link: "https://destinyrecipes.com",
+      group: "Games",
+    },
+    {
+      id: "7",
+      title: "D2 Checkpoint",
+      link: "https://d2checkpoint.com",
+      group: "Games",
+    },
+    {
+      id: "8",
+      title: "EldenRing WiKi",
+      link: "https://eldenring.wiki.fextralife.com",
+      group: "Games",
+    },
+    {
+      id: "9",
+      title: "Palia WiKi",
+      link: "https://palia.wiki.gg",
+      group: "Games",
+    },
+    {
+      id: "10",
+      title: "Palia Rummage Pile",
+      link: "https://palia.th.gl/en/rummage-pile",
+      group: "Games",
+    },
     // Add more shortcuts as needed
   ];
 
